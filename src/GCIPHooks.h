8475e0758518216a294e91d1a343a97861b87fe6
Andrej E Baranov <andrey@andb.name>
2024-05-05 03:04:58 +0700

    debug

---
+++ src/GCIPHooks.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <GCIPConfig.h>
+
+namespace GCIP {
+    void installHooks();
+    void installEventSink();
+    void installInputEventSink();
+    void installRMHooks();
+}
